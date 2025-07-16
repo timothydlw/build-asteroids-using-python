b8401891710cc9292e1b0881eb5ee412de59287b
timothydlw <watsonltim@gmail.com>
2025-07-16 13:26:22 -0500

    Building Asteroids using Python CH2 L6

--- player.py
+++ player.py
@@ -17,4 +17,13 @@ class Player(CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
         
